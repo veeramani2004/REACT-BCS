a92aad11a8bd040ca8cbb525080d9da243f86d3c
veeramani2004 <veeramanipalanisamy642004@gmail.com>
2025-08-12 12:48:57 +0530

    search button added

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -2,18 +2,32 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import { useEffect, useState } from "react";
 import { Movie } from "../components/Movie";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
 
 export function MovieList() {
   const [movielist, setMovielist] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  async function getMovies() {
-    const response = await fetch(
-      "https://68959016039a1a2b288f7c62.mockapi.io/movies",
-      { method: "GET" }
-    );
+  async function getMovies(searchTerm = "") {
+    const url = new URL("https://68959016039a1a2b288f7c62.mockapi.io/movies");
+
+    if (searchTerm) {
+      url.searchParams.append("search", searchTerm);
+    }
+
+    const response = await fetch(url, { method: "GET" });
     const data = await response.json();
     setMovielist(data);
   }
+  // async function getMovies() {
+  //   const response = await fetch(
+  //     "https://68959016039a1a2b288f7c62.mockapi.io/movies",
+  //     { method: "GET" }
+  //   );
+  //   const data = await response.json();
+  //   setMovielist(data);
+  // }
 
   useEffect(() => {
     getMovies();
@@ -30,27 +44,46 @@ export function MovieList() {
     console.log("Status:", response.status);
     const data = await response.json();
     console.log("Response data:", data);
-    getMovies();
+    getMovies(searchTerm);
+  };
+  const searchMovies = (event) => {
+    event.preventDefault();
+    console.log("Searchinggg...", searchTerm);
+    getMovies(searchTerm);
   };
 
   return (
-    <div className="movie-list-container">
-      {movielist.map((movie) => (
-        <Movie
-          key={movie.id}
-          movie={movie}
-          deleteBtn={
-            <IconButton
-              aria-label="delete"
-              color="error"
-              onClick={() => deleteMovie(movie.id)}
-            >
-              <DeleteIcon />
-            </IconButton>
-            // <button onClick={() => deleteMovie(movie.id)}>🗑️ Delete Me</button>
-          }
+    <div>
+      <form onSubmit={searchMovies} className="add-movie-form">
+        <TextField
+          label="Search"
+          slotProps={{
+            input: {
+              startAdornment: <SearchIcon />,
+            },
+          }}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          value={searchTerm}
         />
-      ))}
+      </form>
+
+      <div className="movie-list-container">
+        {movielist.map((movie) => (
+          <Movie
+            key={movie.id}
+            movie={movie}
+            deleteBtn={
+              <IconButton
+                aria-label="delete"
+                color="error"
+                onClick={() => deleteMovie(movie.id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 }
